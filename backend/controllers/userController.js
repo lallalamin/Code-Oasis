29218e4333bc358291f80ca898b4de4cf372ebdf
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-09-28 17:23:38 -0500

    login and logout route is done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,18 +42,40 @@ const signupUser = async(req, res) => {
         res.status(500).json({ message: err.message });
         console.log("Error in signupUser: ", err.message);
     }
-}
+};
 
 const loginUser = async(req, res) => {
     try {
         const { username, password } = req.body;
-        const user = await findOne({ username });
-        //const isPasswordCode
+        const user = await User.findOne({ username }); //when the user is not found, you cannot reach to password of undefined
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || ""); // we put "" because we don't want to compare existing password with a null
+
+        if(!user || !isPasswordCorrect) return res.status(400).json({message: "Invalid username or password"});
+
+        generateTokenAndsetCookie(username._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            username: user.username,
+        });
         
     } catch (error) {
         res.status(500).json({message: error.message});
         console.log("Error in loginUser: ", error.message);
     }  
-}
+};
 
-export { signupUser, loginUser }
+const logoutUser = (req, res) =>{
+    try {
+        res.cookie("jwt", "", {maxAge: 1});
+        res.status(200).json({message: "User logged out successfully"});
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in loginUser: ", error.message);
+    }
+};
+
+export { signupUser, loginUser, logoutUser }
