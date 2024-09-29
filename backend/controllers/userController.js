70930835fae4c41beee599064fd7ab239c7362aa
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-09-28 19:48:31 -0500

    updateuser route is done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -111,7 +111,33 @@ const followUnfollowUser = async(req, res) => {
 };
 
 const updateUser = async(req, res) =>{
+    const { name, email, username, password, profilePic, bio} = req.body;
+    const userId = req.user._id;
+    try {
+        let user = await User.findById(userId);
+        if(!user) return res.status(400).json({message: "User not found"});
 
+        if(req.params.id !== userId.toString()) return res.status(400).json({message: "You cannot update other user's profile"}) // we need to convert to string because the userId is an object
+
+        if(password){
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            user.password = hashedPassword;
+        }
+
+        user.name = name || user.name;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.profilePic = profilePic || user.profilePic;
+        user.bio = bio || user.bio;
+        user = await user.save();
+
+        res.status(200).json({message: "Profile updated successfully", user});
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in updateUser: ", error.message);
+    }
 }
 
 export { signupUser, loginUser, logoutUser, followUnfollowUser, updateUser }
