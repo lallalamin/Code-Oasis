c85e2aded3cb19f0c9f37c2feed0fe2c2fbfebaa
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-10-02 23:41:19 -0500

    createPost route is created

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { createPost } from "../controllers/postController.js";
+import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/create", createPost)
+router.post("/create", protectRoute, createPost)
 
 export default router;
