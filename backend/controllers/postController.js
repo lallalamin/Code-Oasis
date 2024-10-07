c5f39cc318f7b9e6ee8fa2a204e02e2eb4b2c6c3
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-10-06 23:41:25 -0500

    deletePost route is done

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -38,7 +38,7 @@ const getPost = async(req, res) => {
     try {
         const post = await Post.findById(req.params.id);
         if(!post){
-            res.status(404).json({ message: "Post not found" });
+            return res.status(404).json({ message: "Post not found" });
         }
 
         res.status(200).json({ message: "Post Found", post });
@@ -49,4 +49,24 @@ const getPost = async(req, res) => {
     }
 };
 
-export { createPost, getPost };
+const deletePost = async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        if(post.postedBy.toString() !== req.user._id.toString()){
+            return res.status(401).json({ message: "Unauthorized to delete post"});
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Post deleted successfully", post });
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log("Error in deletePost: ", error.message);  
+    }
+} 
+
+export { createPost, getPost, deletePost };
