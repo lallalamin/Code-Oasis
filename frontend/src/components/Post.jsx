6295ce690147ea8a131a71b9636d1c6f8c6c8763
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-11-18 22:25:13 -0600

    when profile pic is click user will be direct to the profile page

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -5,12 +5,14 @@ import { Link } from 'react-router-dom'
 import Actions from './Actions'
 import useShowToast from '../hooks/useShowToast'
 import { set } from 'mongoose'
+import { useNavigate } from 'react-router-dom'
 
 
 const Post = ({post, postedBy}) => {
     const [liked, setLiked] = useState(false);
     const showToast = useShowToast();
     const [user, setUser] = useState(null);
+    const navigare = useNavigate();
 
     useEffect(() => {
         const getUser = async() => {
@@ -39,10 +41,13 @@ const Post = ({post, postedBy}) => {
 
   return (
     <>
-    <Link to={"/markzuckerberg/post/1"}>
+    <Link to={`/${user.username}/post/4{post._id}`}>
         <Flex gap={3} mb={4} py={5}>
             <Flex flexDirection={"column"} alignItems={"center"}>
-                <Avatar size="md" name={user.name} src={user?.profilePic}></Avatar>
+                <Avatar size="md" name={user.name} src={user?.profilePic} onClick={(e) => {
+                    e.preventDefault();
+                    navigare(`/${user.username}`);
+                }}></Avatar>
                 {/* <Box w="1px" h={"full"} bg={"gray.light"} my={2}></Box>
                 <Box position={"relative"} w={"full"}>
                     <Avatar size={"xs"} name='John Doe' src='https://bit.ly/dan-abramov' position={"absolute"} top={"0px"} left={"15px"} padding={"2px"}/>
@@ -53,7 +58,10 @@ const Post = ({post, postedBy}) => {
             <Flex flex={1} flexDirection={"column"} gap={2}>
                 <Flex justifyContent={"space-between"} w={"full"}>
                     <Flex w={"full"} alignItems={"center"}>
-                        <Text fontSize={"sm"} fontWeight={"bold"}>{user?.username}</Text>
+                        <Text fontSize={"sm"} fontWeight={"bold"} onClick={(e) => {
+                        e.preventDefault();
+                        navigare(`/${user.username}`);
+                    }}>{user?.username}</Text>
                         <Image src='/verified.png' w={4} h={4} ml={1}/>
                     </Flex>
                     <Flex gap={4} alignItems={"center"}>
