4d4f9c33e712b1572a3dc8a4b7b47bf1b9ca327f
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-11-24 16:21:43 -0600

    Reply function is working now

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from "react";
-import {  Box, Flex, Text } from '@chakra-ui/react'
+import {  Box, Flex, Text, Modal, ModalOverlay, ModalContent, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, ModalHeader, FormControl, Input, Button } from '@chakra-ui/react'
 import { useRecoilValue } from 'recoil'
 import userAtom from '../atoms/userAtom'
 import useShowToast from "../hooks/useShowToast";
@@ -8,18 +8,24 @@ import useShowToast from "../hooks/useShowToast";
 const Actions = ({ post:post_ }) => {
 	const user = useRecoilValue(userAtom);
 	const [liked, setLiked] = useState(post_.likes.includes(user?._id));
-	const showToast = useShowToast();
 	const [post, setPost] = useState(post_);
+	const [isLiking, setIsLiking] = useState(false);
+	const [isReplying, setIsReplying] = useState(false);
+	const [reply, setReply] = useState("");
+	
+	const showToast = useShowToast();
+	const { isOpen, onOpen, onClose } = useDisclosure()
 
 	const handleLikeAndUnlike = async() => {
 		if(!user) return showToast("Error", "Please login to like/unlike posts", "error");
+		if (isLiking) return;
+		setIsLiking(true);
 		try {
 			const res = await fetch("/api/posts/like/" + post._id, {
 				method: "PUT",
 				headers: {
 					"Content-Type": "application/json",
 				},
-				body: JSON.stringify({ userId: user._id }),
 			});
 			
 			const data = await res.json();
@@ -39,8 +45,41 @@ const Actions = ({ post:post_ }) => {
 			setLiked(!liked);
 		} catch (error) {
 			showToast("Error", error.message, "error");
+		} finally {
+			setIsLiking(false);
 		}
 	}
+
+	const handleReply = async() => {
+		if(!user) return showToast("Error", "Please login to reply to posts", "error");
+		if(isReplying) return;
+		setIsReplying(true);
+		try {
+			const res = await fetch("/api/posts/reply/" + post._id, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ text: reply }),
+			});
+			const data = await res.json();
+
+			if (data.error) {
+				return showToast("Error", data.error, "error");
+			}
+
+			setPost({ ...post, replies: [...post.replies, data.reply] });
+			showToast("Success", "Reply added successfully", "success");
+			console.log(data);
+			onClose();
+			setReply("");
+		} catch (error) {
+			showToast("Error", error.message, "error");
+		} finally{
+			setIsReplying(false);
+		}
+	}
+
 	return (
 		<Flex flexDirection={"column"}>
 			<Flex gap={3} my={2} onClick={(e) => e.preventDefault()}>
@@ -61,7 +100,9 @@ const Actions = ({ post:post_ }) => {
 					></path>
 				</svg>
 
-				<svg aria-label='Comment' color='' fill='' height='20' role='img' viewBox='0 0 24 24' width='20'>
+				<svg aria-label='Comment' color='' fill='' height='20' role='img' viewBox='0 0 24 24' width='20'
+					onClick={onOpen}
+				>
 					<title>Comment</title>
 					<path
 						d='M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z'
@@ -71,8 +112,53 @@ const Actions = ({ post:post_ }) => {
 						strokeWidth='2'
 					></path>
 				</svg>
+				<RepostSVG/>
+				<ShareSVG/>
 
-				<svg
+			</Flex>
+			<Flex gap={2} alignItems={"center"}>
+					<Text color={"gray.light"} fontSize={"sm"}> {post.replies.length} replies</Text>
+					<Box w={0.5} h={0.5} bg={"gray.light"} borderRadius={"full"}></Box>
+					<Text color={"gray.light"} fontSize={"sm"}> {post.likes.length} likes</Text>
+			</Flex>
+
+			<Modal
+				isOpen={isOpen}
+				onClose={onClose}
+			>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader></ModalHeader>
+				<ModalCloseButton />
+				<ModalBody pb={6}>
+					<FormControl>
+					<Input placeholder='Reply goes here...' 
+						value={reply}
+						onChange={(e) => setReply(e.target.value)}
+					/>
+					</FormControl>
+				</ModalBody>
+
+				<ModalFooter>
+					<Button colorScheme='blue' size={'sm'} mr={3}
+						onClick={handleReply}
+						isLoading={isReplying}
+					>
+					Reply
+					</Button>
+				</ModalFooter>
+				</ModalContent>
+			</Modal>
+
+		</Flex>
+	);
+};
+
+export default Actions;
+
+const RepostSVG = () => {
+	return (
+		<svg
 					aria-label='Repost'
 					color='currentColor'
 					fill='currentColor'
@@ -87,8 +173,11 @@ const Actions = ({ post:post_ }) => {
 						d='M19.998 9.497a1 1 0 0 0-1 1v4.228a3.274 3.274 0 0 1-3.27 3.27h-5.313l1.791-1.787a1 1 0 0 0-1.412-1.416L7.29 18.287a1.004 1.004 0 0 0-.294.707v.001c0 .023.012.042.013.065a.923.923 0 0 0 .281.643l3.502 3.504a1 1 0 0 0 1.414-1.414l-1.797-1.798h5.318a5.276 5.276 0 0 0 5.27-5.27v-4.228a1 1 0 0 0-1-1Zm-6.41-3.496-1.795 1.795a1 1 0 1 0 1.414 1.414l3.5-3.5a1.003 1.003 0 0 0 0-1.417l-3.5-3.5a1 1 0 0 0-1.414 1.414l1.794 1.794H8.27A5.277 5.277 0 0 0 3 9.271V13.5a1 1 0 0 0 2 0V9.271a3.275 3.275 0 0 1 3.271-3.27Z'
 					></path>
 				</svg>
-
-				<svg
+	);
+}
+const ShareSVG = () => {
+	return (
+		<svg
 					aria-label='Share'
 					color=''
 					fill='rgb(243, 245, 247)'
@@ -116,14 +205,5 @@ const Actions = ({ post:post_ }) => {
 						strokeWidth='2'
 					></polygon>
 				</svg>
-				<Flex gap={2} alignItems={"center"}>
-					<Text color={"gray.light"} fontSize={"sm"}> {post.replies.length} replies</Text>
-					<Box w={0.5} h={0.5} bg={"gray.light"} borderRadius={"full"}></Box>
-					<Text color={"gray.light"} fontSize={"sm"}> {post.likes.length} likes</Text>
-				</Flex>
-			</Flex>
-		</Flex>
 	);
-};
-
-export default Actions;
+}
