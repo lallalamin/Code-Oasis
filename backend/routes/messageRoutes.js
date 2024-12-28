70e8ee089f016acd91325e873b662e396865052b
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-12-27 22:18:20 -0600

    added new route

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
-import { sendMessage, getMessages } from "../controllers/messageController.js";
+import { sendMessage, getMessages, getConversations } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, sendMessage);
 router.get("/:otherUserId", protectRoute, getMessages);
-
+router.get("/conversations", protectRoute, getConversations);
 
 export default router;
