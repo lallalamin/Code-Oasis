214116668f6c72d98f1efc9f3677c666794704d4
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2025-01-21 23:49:18 -0600

    deployment

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,14 @@ import postRoutes from './routes/postRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import {v2 as cloudinary} from 'cloudinary';
 import { app, server } from './socket/socket.js';
+import path from 'path';
 
 dotenv.config(); // to be able to read the content in the .env file
 
 connectDB(); 
 
 const PORT = process.env.PORT || 5000;
+const _dirname = path.resolve();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -29,6 +31,15 @@ app.use(cookieParser()); // to read and access the cookie
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
+
+// http://localhost:5000 => both backend and frontend
+
+if(process.env.NODE_ENV === "production") { // if we are in production mode
+    app.use(express.static(path.join(_dirname, "/frontend/dist")));
+    app.get("*", (req, res) => { // to serve the react app in production mode
+        res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 
 server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT} heyy`));
