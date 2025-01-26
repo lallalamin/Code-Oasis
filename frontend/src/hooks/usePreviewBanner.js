637c1d1d3a57fdd4bd22fd7e63b9da0b92735c57
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2025-01-25 19:42:45 -0600

    create a hook for banner preview

---
+++ frontend/src/hooks/usePreviewBanner.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import useShowToast from './useShowToast';
+
+const usePreviewBanner = () => {
+  const [imgUrl, setImgUrl] = useState(null);
+  const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+  const showToast = useShowToast();
+  const handleBannerChange = (e) => {
+    const file = e.target.files[0];
+    if(!file) return;
+
+    if(!allowedTypes.includes(file.type)) {
+        showToast("File format not supported", "Please select an image file", "error");
+        setImgUrl(null);
+        return;
+    }
+    
+    if(file && file.type.startsWith('image/')) {
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImgUrl(reader.result);
+        }
+        reader.readAsDataURL(file); // this will take our file that we selected, turn it to base 64 string. take that string and render it in our image.
+    }
+    else{
+        showToast("File format not supported", "Please select an image file", "error");
+        setImgUrl(null);
+    }
+  }
+  return { handleBannerChange, imgUrl, setImgUrl }
+}
+
+export default usePreviewBanner
