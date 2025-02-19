3314ea86b4c4617a4dc00c3dc707ca3d8b02b138
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2025-02-18 22:49:18 -0600

    WIP: addeventmodal

--- frontend/src/components/AddEventModal.jsx
+++ frontend/src/components/AddEventModal.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, FormControl, FormLabel, Input, Text } from '@chakra-ui/react'
 
 const AddEventModal = () => {
+    const [isLoading, setIsLoading] = useState(false);
   return (
-    <div>AddEventModal</div>
+    <>
+        <Button leftIcon={<FaPlus />} size="sm" onClick={onOpen}>
+        Add Event
+        </Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+            <ModalHeader>Add New Event</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+            <FormControl>
+                <FormLabel>Event Title</FormLabel>
+                <Input
+                value={title}
+                onChange={handleTextChange}
+                placeholder="Enter task title"
+                />
+                <Text fontSize={"sm"} mt={2}>
+                {remainingChar}/{MAX_CHAR}
+                </Text>
+            </FormControl>
+            </ModalBody>
+            <ModalFooter>
+            <Button colorScheme="blue" isLoading={loading}>
+                Add Event
+            </Button>
+            </ModalFooter>
+        </ModalContent>
+        </Modal>
+    </>
   )
 }
 
