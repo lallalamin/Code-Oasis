4b88239ecf7341fccdbb38194497ebce9e860aaf
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2025-03-15 23:16:25 -0500

    UI layout for news card done

--- frontend/src/components/New.jsx
+++ frontend/src/components/New.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Flex, Text, Image } from '@chakra-ui/react'
+import { useColorModeValue } from '@chakra-ui/react';
 
 const New = () => {
   return (
-    <div>New</div>
+    <Flex flexDirection={"column"} justifyContent="center" >
+      <Image src="https://placehold.co/600x400" mb={2} />
+      <Flex gap={2} flexDirection={"row"} justifyContent={"space-between"} w={"full"}> 
+        <Text fontSize={"sm"} color={useColorModeValue("gray.light", "gray.500")}>John Doe</Text>
+        <Text fontSize={"sm"} color={useColorModeValue("gray.light", "gray.500")}>May 25, 2025</Text>
+      </Flex>
+      <Text fontSize={"xl"} fontWeight={"bold"}>Title of the news</Text>
+      <Text>Description of the news. It will probably have to be longer ...</Text>
+    </Flex>
   )
 }
 
--- frontend/src/pages/NewsPage.jsx
+++ frontend/src/pages/NewsPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Flex, Text, Image } from '@chakra-ui/react'
+import { Flex, Text, Image, SimpleGrid } from '@chakra-ui/react'
+import New from '../components/New'
 
 const NewsPage = () => {
   return (
     <>
-      <Flex justifyContent="center">
-          <Text fontWeight={"bold"}>Recent News</Text>
+      <Flex direction="column" alignItems={"center"} justifyContent="center">
+          <Text fontSize={"2xl"} fontWeight={"bold"} mb={8}>Recent News</Text>
+          <SimpleGrid columns={{base: 1, md: 2}} spacing={5}>
+            <New/>
+            <New/>
+            <New/>
+            <New/>
+          </SimpleGrid>
       </Flex>
     </>
   )
