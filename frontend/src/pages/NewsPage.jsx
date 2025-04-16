81f6c15a6eaa0c3bc510607e46e9ff0d735e58e5
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2025-04-15 23:37:09 -0500

    Added disclaimer at the bottom of news page

--- frontend/src/pages/NewsPage.jsx
+++ frontend/src/pages/NewsPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Flex, Text, Image, SimpleGrid } from '@chakra-ui/react'
+import { Flex, Text, Image, SimpleGrid, Box } from '@chakra-ui/react'
 import New from '../components/New'
+import { useColorModeValue } from '@chakra-ui/react';
 
 const NewsPage = () => {
   return (
@@ -13,6 +14,22 @@ const NewsPage = () => {
             <New/>
             <New/>
           </SimpleGrid>
+          <Box w="full"
+            bg="rgba(255, 255, 255, 0.1)"
+            boxShadow="0px 0px 15px rgba(0,0,0,0.09)"
+            p="6"
+            borderRadius="lg"
+            mb={8}
+            mt={8}
+            >
+            <Text fontSize={"md"} fontWeight={"bold"} mb={2}>
+              📰 Hugo&apos;s been busy! He collected tech news from website for you!
+            </Text>
+            <Text fontSize={"sm"} color={useColorModeValue("gray.600", "gray.400")}>
+              Articles come from amazing sites like TechCrunch and VentureBeat — we don&apos;t own them, we&apos;re just sharing what&apos;s cool.
+              Click through to read the full stories on their original sites!
+            </Text>
+          </Box>
       </Flex>
     </>
   )
