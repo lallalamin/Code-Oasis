0c8acd93133160fca245711f11c618c4dca4ceb7
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2025-04-16 23:24:27 -0500

    fix import error

--- backend/cron/scrapeNews.js
+++ backend/cron/scrapeNews.js
@@ -1,5 +1,5 @@
 import cron from "node-cron";
-import { scrapeTechNews } from "../controllers/newsController";
+import { scrapeTechNews } from "../controllers/newsController.js";
 
 const scrapeNewsJob = cron.schedule("0 6 * * *", async () => {
     await scrapeTechNews();
